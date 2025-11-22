f7be0b1e78870d25e845e0cd6368b88533dc7316
Frost <anf5726@students.uc-bcf.edu.ph>
2025-11-22 15:35:44 +0800

    start UI ---> to customer and pharmacy login UI.

--- app/src/main/java/com/example/medfinder/CustomerLoginFragment.kt
+++ app/src/main/java/com/example/medfinder/CustomerLoginFragment.kt
@@ -2,9 +2,6 @@ package com.example.medfinder
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
